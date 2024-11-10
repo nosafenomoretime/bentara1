01764dba93a9c21b74c47eafdf15bb888983a7f4
nosafenomoretime <nosafenomoretime@outlook.com>
2024-11-10 14:22:10 +0000

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 chmod +x ngrok
-./ngrok authtoken 22Xpmisx3uIlZUUk20XLN6NJEDE7_3KrVLvDPp6wjo81FwYsK8
+./ngrok authtoken 2XpmShntfbuh3wyrFZHT4Ux7Ocl_22riFJ8s3sq4JqRrhT5Dn
 ./ngrok tcp 44567 >/dev/null 2>&1 >/dev/null &
 
 sleep 5
